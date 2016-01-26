527e13e419ed42db48617be6ae7196bdab8870e6
Charles Carter and Jacob Steinberg <charles.h.carter@gmail.com>
2016-01-25 16:41:20 -0800

    add styles and minor effects and remove() refactor

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,18 @@ function Place (local,landmark,season,notes) {
   this.landmark = landmark;
   this.season = season;
   this.notes = notes;
+
+  function resetFields(){
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+  }
 }
 
 Place.prototype.fullLand = function () {
-  return this.local + " " + this.landmark;
+  return this.season + " in " + this.local;
 }
 
 $(document).ready(function() {
@@ -19,7 +27,6 @@ $(document).ready(function() {
    var inputtedLandmark = $("input#new-landmark").val();
    var inputtedSeason = $("input#new-season").val();
    var inputtedNotes = $("input#new-notes").val();
-
   var newLocal = new Place(inputtedLocal, inputtedLandmark, inputtedSeason, inputtedNotes);
 
    $("ul#locationList").append("<li><span class='contact'>" + newLocal.fullLand() + "</span></li>");
@@ -30,20 +37,16 @@ $(document).ready(function() {
    $("input#new-notes").val("");
 
    $(".contact").last().click(function(){
-     $("#show-location").toggle();
+     $("#show-location").fadeToggle();
      $("#show-location h2").text(newLocal.fullLand());
      $(".first-location").text(newLocal.local);
      $(".season").text(newLocal.season);
      $(".landmarked").text(newLocal.landmark);
      $(".noted").text(newLocal.notes);
 
+     $resetFields();
   });
 
-  $("input#new-first-name").val("");
-  $("input#new-last-name").val("");
-  $("input.new-street").val("");
-  $("input.new-city").val("");
-  $("input.new-state").val("");
 
 });
 });
